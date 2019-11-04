af3c5b3fa229e8e2fbbbcc3c90409ac2ba2f08bf
kadamsagar039 <40513747+kadamsagar039@users.noreply.github.com>
2019-11-04 16:26:51 +0530

    Update settings.py

--- fundoo/fundoo/settings.py
+++ fundoo/fundoo/settings.py
@@ -100,14 +100,14 @@ CHANNEL_LAYERS = {
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-#DATABASES = {
-#    'default': {
- #       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-  ###    'PASSWORD': '1qazXSW@#EDC',
-     #   'HOST': 'db1237.postgres.database.azure.com',
-      #  'PORT': '5432',
-    #}
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'PASSWORD': '1qazXSW@#EDC',
+        'HOST': 'db1237.postgres.database.azure.com',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
